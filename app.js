dc01496ee54a704354eaef7aa7bec291886ae955
axelnoerr <axelnoerodriguezgonzalez@gmail.com>
2026-05-31 22:04:39 -0600

    Update app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+function mostrarSeccion(id){
+
+    document
+        .querySelectorAll(".seccion")
+        .forEach(sec => sec.classList.remove("activa"));
+
+    document
+        .getElementById(id)
+        .classList.add("activa");
+}
